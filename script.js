c9827f527fc54ea752a497181d0bf32c35979d3e
maaalin <78012771+maaalin@users.noreply.github.com>
2021-02-10 19:49:43 +0100

    working on that rwd

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 
+// A $( document ).ready() block.
+// $( document ).ready(function() {
+//     alert( "ready!" );
+// });
+
 $(document).ready(function(){
     $("#backpacking").hover(function()
     {
@@ -34,3 +39,30 @@ $(".button").hover(function(){
 $(".dropdown").slideToggle();
 });
 });
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+1
+2
+3
+4
