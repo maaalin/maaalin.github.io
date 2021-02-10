3c9f72c268195b2352a16a72952e971caf934773
maaalin <78012771+maaalin@users.noreply.github.com>
2021-02-10 20:31:32 +0100

    rwd 3

--- script.js
+++ script.js
@@ -15,23 +15,7 @@ $(document).ready(function(){
     });
 });
 
-$(document).ready(function(){
-    $("#nay").hover(function()
-    {
-        $("#nay").css("color", "#e9b0b5");
-    }, function()
-    {
-        $("#nay").css("color", "black");
-    });
-});
 
-function mouseOver() {
-  document.getElementById("om").style.color = "#e9b0b5";
-}
-
-function mouseOut() {
-  document.getElementById("om").style.color = "black";
-}
 
 
 $(document).ready(function(){
