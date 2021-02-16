f0826402eab9ae6979c1dc71ad28846ba0d08864
maaalin <78012771+maaalin@users.noreply.github.com>
2021-02-16 15:02:44 +0100

    mobile2

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
 
   $(window).resize(function(){
-    if($(window).width() > 980){
+    if($(window).width() > 1080){
       $("ul").removeAttr('style');
     }
   });
