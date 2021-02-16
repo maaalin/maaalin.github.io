3084bc6d7516d762668b3687b4f267e6d4f4f742
maaalin <78012771+maaalin@users.noreply.github.com>
2021-02-16 12:44:22 +0100

    mobile

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
-//
-//
-// $( document ).ready(function() {
-//     alert( "ready!" );
-// });
+$(document).ready(function() {
 
 
-$( "img" ).hide();
+  $("nav div").click(function(){
+    $("ul").slideToggle();
+    $("ul ul").css("display", "none");
+  });
+
+  $('ul li').click(function () {
+    $(this).siblings().find('ul').slideUp();
+    $(this).find('ul').slideToggle();
+  });
+
+
+
+
+  $(window).resize(function(){
+    if($(window).width() > 980){
+      $("ul").removeAttr('style');
+    }
+  });
 
 
 
 
 
 
-$(document).ready(function(){
-$(".button").hover(function(){
-$(".dropdown").slideToggle();
+
+
 });
-});
-
-
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
