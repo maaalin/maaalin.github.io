1801c944e12f6b5b9cbdb91c77fd9974341dbf79
maaalin <78012771+maaalin@users.noreply.github.com>
2021-03-06 17:58:07 +0100

    moved menu

--- js/script.js
+++ js/script.js
@@ -22,6 +22,13 @@ $(document).ready(function() {
 
 
 
+  $(window).scroll(function () {
+    if($(window).scrollTop() > 100) {
+      $(".up").addClass('upShow');
+    } else {
+      $(".up").removeClass('upShow');
+    }
+  });
 
 
 
