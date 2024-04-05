e624856dd0cb6a55094757374e35006fdeeee577
maaalin <malinxbengtsson@hotmail.com>
2024-04-05 22:09:08 +1100

    many many updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //copyright i footer
 var paragraph = document.getElementById("year");
-var text = document.createTextNode(" 2023 Malin Bengtsson");
+var text = document.createTextNode(" 2024 Malin Bengtsson");
 
 paragraph.appendChild(text);
 //slut copyright
